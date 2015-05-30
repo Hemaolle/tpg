d63cff6b80566635d23befaf96ab21ca46b1ca1e
Oskari <oskari.leppaaho@gmail.com>
2015-05-30 22:45:42 +0300

    Some html/css/javascript testing: colors, button, dropdown list

---
+++ EaselJS/script.js
@@ -0,0 +1,6 @@
+function clickTest () {
+	// window.alert("I am an alert box!")
+	document.getElementById("redId").innerHTML = "test";
+}
+
+var areas = { "Market", "Courthouse", "Church", "Circus" }
