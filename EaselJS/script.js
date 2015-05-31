28db2f1e2384156cefa0e40bc462a49451b8775e
Oskari <oskari.leppaaho@gmail.com>
2015-05-31 08:26:07 +0300

    The player is at a location and the location can be changed from a dropdown

--- EaselJS/script.js
+++ EaselJS/script.js
@@ -1,6 +1,26 @@
-function clickTest () {
-	// window.alert("I am an alert box!")
+var currentLocation;
+
+function clickTest() {
+	// window.alert("I am an alert box!");
 	document.getElementById("redId").innerHTML = "test";
 }
 
-var areas = { "Market", "Courthouse", "Church", "Circus" }
+var areas = ["Market", "Courthouse", "Church", "Circus"];
+
+window.onload = start;
+
+function start () {
+	for(var index in areas){
+		placeDropDown.options[index] =
+			new Option(areas[index], areas[index]);
+	}
+	document.getElementById("redId").innerHTML = "start";
+	document.getElementById("currentLocation").innerHTML = areas[0];
+	currentLocation = areas[0];
+}
+
+function onLocationChange(val) {
+	// window.alert("location changed to " + val);
+	currentLocation = val;
+	document.getElementById("currentLocation").innerHTML = currentLocation;
+}
