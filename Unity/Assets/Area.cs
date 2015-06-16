9a8aea0ac5c41453dca893fe020d145153b24eae
Oskari <oskari.leppaaho@gmail.com>
2015-06-16 17:41:53 +0300

    Created empty classes for Areas, Locations and Players

---
+++ Unity/Assets/Area.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// An area where the player is located. For exapmle a shop or
+// the city hall
+public class Area {
+
+}
---
+++ Unity/Assets/Location.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// A location is for example a city, contains multiple areas
+public class Location {
+
+}
