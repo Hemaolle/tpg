6e619d38b42c43b5df0647ba79f35d8c34dcdc39
Oskari <oskari.leppaaho@gmail.com>
2015-06-16 18:23:38 +0300

    Added a world class

---
+++ Unity/Assets/Scripts/Model/World.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Dystopia.Model {
+
+	public class World {
+
+		public List<Location> Locations {
+			get;
+			set;
+		}
+	}
+}
