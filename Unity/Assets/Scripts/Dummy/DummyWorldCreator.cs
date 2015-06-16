2bcb65028e5c1be58893efe61fcd26ce7e1a5a75
Oskari <oskari.leppaaho@gmail.com>
2015-06-16 18:56:48 +0300

    Started working on a DummyWorldCreator

---
+++ Unity/Assets/Scripts/Dummy/DummyWorldCreator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using Dystopia.Model;
+using System.Collections.Generic;
+
+namespace Dystopia.Dummy {
+	public class DummyWorldCreator : MonoBehaviour {
+		public World dummyWorld;
+
+		// Use this for initialization
+		void Start () {
+			dummyWorld = new DummyWorld();
+			DummyLocation location1 = new DummyLocation(new Vector2(30,30));
+			DummyLocation location2 = new DummyLocation(new Vector2(200,250));
+
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+}
