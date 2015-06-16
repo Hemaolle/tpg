1cbafeb1de5e6b0b22322103601f9e8d8b8d8fe8
specnats <specnats@hotmail.fi>
2015-06-16 19:10:02 +0300

    Dummyworld locations listing

--- Unity/Assets/Scripts/Dummy/DummyWorldCreator.cs
+++ Unity/Assets/Scripts/Dummy/DummyWorldCreator.cs
@@ -3,20 +3,32 @@ using System.Collections;
 using Dystopia.Model;
 using System.Collections.Generic;
 
-namespace Dystopia.Dummy {
-	public class DummyWorldCreator : MonoBehaviour {
+namespace Dystopia.Dummy
+{
+	public class DummyWorldCreator : MonoBehaviour
+	{
 		public World dummyWorld;
 
 		// Use this for initialization
-		void Start () {
+		void Start()
+		{
 			dummyWorld = new DummyWorld();
-			DummyLocation location1 = new DummyLocation(new Vector2(30,30));
-			DummyLocation location2 = new DummyLocation(new Vector2(200,250));
-
+			DummyLocation location1 = new DummyLocation(new Vector2(30, 30));
+			DummyLocation location2 = new DummyLocation(new Vector2(200, 250));
+			List<Location> locations = new List<Location>();
+			locations.Add(location1);
+			locations.Add(location2);
+			dummyWorld.Locations = locations;
+			
+            foreach (Location location in dummyWorld.Locations)
+            {
+                Debug.Log(location.Coordinates);
+            }
 		}
 		
 		// Update is called once per frame
-		void Update () {
+		void Update()
+		{
 		
 		}
 	}
