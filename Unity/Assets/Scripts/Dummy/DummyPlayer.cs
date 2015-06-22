4909424e8faa71af17a4ded267807d793bbe1ebb
specnats <specnats@hotmail.fi>
2015-06-22 22:46:10 +0300

    DummyPlayer script

--- Unity/Assets/Scripts/Dummy/DummyPlayer.cs
+++ Unity/Assets/Scripts/Dummy/DummyPlayer.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using Dystopia.Model;
 
-public class DummyPlayer : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+namespace Dystopia.Dummy 
+{
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	[System.Serializable]
+	public class Player {
+		
+		public Area CurrentArea {
+			get;
+			set;
+		}
+		
+		public string Name {
+			get;
+			set;
+		}
+		
+		public void MoveTo(Area area) {
+			
+		}
 	}
 }
