d073e9eb784fed2b618971cd9e0bcf9ad6f86cef
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-07-14 20:05:08 +0300

    Started to work on AreaNavigatorPicture

---
+++ Unity/Assets/Scripts/UI/AreaNavigatorPicture.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AreaNavigatorPicture : MonoBehaviour {
+
+	public Image areaPicture;
+	public GameObject areaLinkButton;
+	public RectTransform panel;
+
+	public AreaData startingArea;
+
+	void SetCurrentArea(AreaData areaData) {
+		areaPicture.sprite = areaData.Picture;
+		foreach (AreaData.AreaLink link in areaData.areaLinks) {
+			GameObject newButton = Instantiate(areaLinkButton) as GameObject;
+			newButton.transform.SetParent(panel);
+			newButton.GetComponent<RectTransform>().anchoredPosition = link.LinkLocation;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		SetCurrentArea (startingArea);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
